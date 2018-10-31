7ab831ee471043aa18d55947cc3931bcbbbc5a51
Katy Deng <deng2017.xq@gmail.com>
2018-10-31 00:16:18 -0700

    setup ejs

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 import config from './config'; 
-// import fs from 'fs'; 
+// import fs from 'fs'; (without using express)
 import apiRouter from './api';
 
 import express from 'express'; 
 
 const server = express(); 
 server.get('/',(req,res)=>{
-  res.send('Hello express')
+  res.render('index.ejs', {
+    content: "this is a content <em>EJS</em>"
+  })
 });
+//setting the view engine to ejs; express will look at the ejs file under the view folder
+server.set('view engine', 'ejs'); 
+//
+server.use('/api', apiRouter); 
+server.use(express.static('public'))
+
+
+
+
+
+server.listen(config.port, ()=> {
+  console.info('Express listening on port ', config.port);
+});
+
+
+//(without using express)
 // server.get('/about.html',(req,res)=>{
 //   fs.readFile('./about.html', (err,data)=>{
 //     res.send(data.toString());
 //   });
 // });
-server.use('/api', apiRouter); 
-server.use(express.static('public'))
-server.listen(config.port, ()=> {
-  console.info('Express listening on port ', config.port);
-});
